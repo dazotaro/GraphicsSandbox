eefa2b38d7efb40d91fd60ca1f98caac83d62cc6
dazotaro <dazotaro@gmail.com>
2014-01-25 07:54:39 +0100

    Added comment

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 // Description : Hello World in C, Ansi-style
 //============================================================================
 
+// Global includes
 #include <iostream>
 
 int main(void)

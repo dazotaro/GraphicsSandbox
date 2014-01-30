cd047edbfd0917356cdeffc4b905d8cc407262f3
dazotaro <dazotaro@gmail.com>
2014-01-31 00:25:08 +0100

    Added tests

--- main.cpp
+++ main.cpp
@@ -6,12 +6,36 @@
 // Description : Hello World in C, Ansi-style
 //============================================================================
 
+// Local includes
+#include "Force.hpp"
+#include "Particle.hpp"
+#include "ParticleSystem.hpp"
+#include "Defs.hpp"
+
 // Global includes
 #include <iostream>
 
+void testForce01()
+{
+	JU::Force force(0, true, 1111);
+
+	JU::f32 pos[3] = {1.1f, 2.2f, 3.3f};
+	JU::f32 vel[3] = {7.1f, 7.2f, 7.3f};
+	JU::Particle particle(17, 333.3, pos, vel, 13);
+
+	force.addParticle(&particle);
+	particle.addForce(&force);
+
+	std::cout << force << std::endl;
+	std::cout << particle << std::endl;
+
+	force.removeParticle(particle.id_);
+	std::cout << force << std::endl;
+}
+
 int main(void)
 {
-	std::cout << "Hello World!!!" << std::endl;
+	testForce01();
 
 	return 0;
 }

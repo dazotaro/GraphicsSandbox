6c628d3e8483c70607ab6748413d8f44f54ec1d4
Dazotaro <dazotaro@gmail.com>
2014-06-23 16:04:36 +0200

    Changed test scenes

--- graphics/test/test_scene.hpp
+++ graphics/test/test_scene.hpp
@@ -102,8 +102,8 @@ static void display()
 static void init(void)
 {
     //scene = new GLSceneNormal(WIDTH, HEIGHT);
-    //scene = new GLSceneLighting(WIDTH, HEIGHT);
-    scene = new GLSceneShadow(WIDTH, HEIGHT);
+    scene = new GLSceneLighting(WIDTH, HEIGHT);
+    //scene = new GLSceneShadow(WIDTH, HEIGHT);
     //scene = new GLSceneParticles(WIDTH, HEIGHT);
     //scene = new GLSceneCometTail(WIDTH, HEIGHT, 100);
     scene->init();

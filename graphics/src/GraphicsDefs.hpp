3d7bb2e04f864381257b3b194280fd9db26f1f75
Dazotaro <dazotaro@gmail.com>
2014-06-25 13:52:13 +0200

    Fixed collision between ifndef macro names (with Defs.hpp)

--- graphics/src/GraphicsDefs.hpp
+++ graphics/src/GraphicsDefs.hpp
@@ -1,5 +1,5 @@
-#ifndef DEFS_HPP_
-#define DEFS_HPP_
+#ifndef GRAPHICSDEFS_HPP_
+#define GRAPHICSDEFS_HPP_
 
 #include <GL/glew.h>
 #include <GL/freeglut_std.h>
@@ -7,4 +7,4 @@
 const int MIDDLE_BUTTON_SCROLL_UP   = 3;
 const int MIDDLE_BUTTON_SCROLL_DOWN = 4;
 
-#endif // DEFS_HPP_
+#endif // GRAPHICSDEFS_HPP_

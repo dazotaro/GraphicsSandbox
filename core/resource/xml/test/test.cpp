5dc500a8f74aed34332c7d6eed2ce79173cbaf36
Dazotaro <dazotaro@gmail.com>
2014-09-23 15:50:26 +0200

    Adding test files to XMLHandler

---
+++ core/resource/xml/test/test.cpp
@@ -0,0 +1,20 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Sep 12, 2014
+ *      Author: jusabiaga
+ */
+
+#include "XMLHandler.hpp"
+
+int main(void)
+{
+    const char* filename = "data/debug.xml";
+
+    boost::property_tree::ptree pt;
+
+    JU::XMLHandler::load(filename, pt);
+    JU::XMLHandler::save("after.xml", pt);
+
+    return 0;
+}

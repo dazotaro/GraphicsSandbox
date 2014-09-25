c85f7d105d0b03d6caf2359077cb44d94e97354f
Dazotaro <dazotaro@gmail.com>
2014-09-25 15:59:39 +0200

    Switch between drivers

--- core/test/test.cpp
+++ core/test/test.cpp
@@ -5,8 +5,8 @@
  *      Author: jusabiaga
  */
 
-#include "test_xml.cpp"
-//#include "test_ptree.cpp"
+//#include "test_xml.cpp"
+#include "test_ptree.cpp"
 
 /* This file is here just to choose among the three different drivers.
     The drivers are hpp files to avoid Eclipse from automatically compiling them causing name collisions */

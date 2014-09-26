e416f0f8eaf6b9ff68f0021c8663c50ad2cd25e8
Dazotaro <dazotaro@gmail.com>
2014-09-26 13:22:57 +0200

    Added the JSON import/exporter and tests

--- core/test/test_ptree.cpp
+++ core/test/test_ptree.cpp
@@ -7,6 +7,7 @@
 
 #include "TestClass.hpp"
 #include "XMLHandler.hpp"
+#include "JSONHandler.hpp"
 
 
 void testBaseClass()
@@ -72,11 +73,55 @@ void testDerivedClass()
 
 
 
+void testXML2JSON()
+{
+    const char* input_filename = "data/ptree_aggregation.xml";
+    const char* output_filename = "ptree_xml2json_after.json";
+
+    boost::property_tree::ptree pt;
+    boost::property_tree::ptree pt2;
+
+    JU::XMLHandler::load(input_filename, pt);
+
+    AggregationClass object;
+    pt = pt.get_child(object.getTag());
+
+    object.importFromPropertyTree(pt);
+    object.exportToPropertyTree("", pt2);
+
+    JU::JSONHandler::save(output_filename, pt2);
+}
+
+
+
+void testJSON2XML()
+{
+    const char* input_filename = "data/ptree_aggregation.json";
+    const char* output_filename = "ptree_json2xml_after.xml";
+
+    boost::property_tree::ptree pt;
+    boost::property_tree::ptree pt2;
+
+    JU::JSONHandler::load(input_filename, pt);
+
+    AggregationClass object;
+    pt = pt.get_child(object.getTag());
+
+    object.importFromPropertyTree(pt);
+    object.exportToPropertyTree("", pt2);
+
+    JU::XMLHandler::save(output_filename, pt2);
+}
+
+
+
 int main(void)
 {
     testBaseClass();
     testAggregationClass();
     testDerivedClass();
+    testXML2JSON();
+    testJSON2XML();
 
     return 0;
 }

7b64667f08cc2b6b8a6e366901709f854d93ebdf
dazotaro <dazotaro@gmail.com>
2014-11-02 18:51:51 +0100

    Filled in function for traversing a tree

--- core/test/test_hash.cpp
+++ core/test/test_hash.cpp
@@ -5,17 +5,35 @@
  *      Author: jusabiaga
  */
 
+// Local includes
 #include "XMLHandler.hpp"
+
+// Global includes
+#include <cstdio>	// std::printf
+
+void printTree(boost::property_tree::ptree pt)
+{
+
+    for (boost::property_tree::ptree::const_iterator node_iter = pt.begin(); node_iter != pt.end(); ++node_iter)
+    {
+    	std::printf("Key = %s\n", node_iter->first.c_str());
+    	std::printf("Key = %s\n", node_iter->second.get_value<std::string>().c_str());
+
+    	printTree(node_iter->second);
+    }
+
+
+}
 
 int test_hash(void)
 {
-    const char* filename = "data/debug.xml";
+    const char* filename = "data/ptree_container.xml";
 
     boost::property_tree::ptree pt;
 
     JU::XMLHandler::load(filename, pt);
 
-    // DEBUG: traverse the tree and output all the data
+    printTree(pt);
 
     // Traverse the tree and compute the hash value of the tree
 

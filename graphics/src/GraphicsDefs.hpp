f5c156a2c976c12dba857bcc083992463b7f6535
dazotaro <dazotaro@gmail.com>
2014-12-19 17:20:12 +0100

    Removed unnecessary includes

--- graphics/src/GraphicsDefs.hpp
+++ graphics/src/GraphicsDefs.hpp
@@ -1,9 +1,6 @@
 #ifndef GRAPHICSDEFS_HPP_
 #define GRAPHICSDEFS_HPP_
 
-#include <GL/glew.h>
-#include <GL/freeglut_std.h>
-
 const int MIDDLE_BUTTON_SCROLL_UP   = 3;
 const int MIDDLE_BUTTON_SCROLL_DOWN = 4;
 

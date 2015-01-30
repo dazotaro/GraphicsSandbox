9c898fe5605a57a369fa506cf7f951fce3eb3993
dazotaro <dazotaro@gmail.com>
2015-01-30 15:45:37 +0100

    Added commented out code (for future (Debuggin) use)

--- graphics/test/shaders/image_texture.fs
+++ graphics/test/shaders/image_texture.fs
@@ -8,5 +8,9 @@ out vec4 outputColor;
 
 void main()
 {
+   //vec4 texColor = texture(tex_image, TexCoord);
+   //int channel = 2;
+   //outputColor = vec4(texColor[channel], texColor[channel], texColor[channel], 1.0f);
+   
    outputColor = texture(tex_image, TexCoord);
 }
